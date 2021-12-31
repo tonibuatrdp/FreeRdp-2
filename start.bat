3cc582bdffb1a5b500ceae887e415026e42360c8
Penjaga Kesdihan <76855943+RamaGanzz@users.noreply.github.com>
2021-12-31 10:34:03 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By MarcelCyan" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By RamaGanz" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator @MarcelCyan /add >nul
+net user administrator @RamaGanzz /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
